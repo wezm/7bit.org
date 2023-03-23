3d904b0019fcef3d045f4f9639f4c8580b197e45
Wesley Moore <wes@wezm.net>
2023-03-23 12:43:21 +1000

    Add MacBinary decoder

---
+++ public/macbinary/macbinary.d.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} val
+* @returns {any}
+*/
+export function parse_macbinary(val: any): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly parse_macbinary: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ public/macbinary/main.js
@@ -0,0 +1,79 @@
+import init, { parse_macbinary } from './macbinary.js';
+
+async function run() {
+    await init();
+}
+
+async function readMacbinary(event, details, error) {
+    event.preventDefault();
+    details.classList.add('hidden');
+    error.classList.add('hidden');
+
+    try {
+        const file = document.forms[0]['file'].files.item(0);
+        const data = await file.arrayBuffer();
+        const res = parse_macbinary(data);
+
+        setField('filename', res.name);
+        setField('type', res.type);
+        setField('creator', res.creator);
+        setField('data_fork_len', res.data_fork.length);
+        const downloadDataLink = document.getElementById('download_data_fork');
+        downloadDataLink.href = URL.createObjectURL(new Blob(
+            [ res.data_fork ],
+            { type: 'application/octet-stream' }
+        ));
+        downloadDataLink.download = res.name;
+        setField('created', new Date(res.created * 1000).toString());
+        setField('modified', new Date(res.modified * 1000).toString());
+
+        const template = document.getElementById('resource');
+        const resourceTable = document.querySelector('#resources table');
+        const tbody = resourceTable.querySelector('tbody');
+        tbody.replaceChildren(); // remove all children
+        for (const rsrc of res.resources) {
+            const tr = template.content.cloneNode(true);
+            const rows = tr.querySelectorAll('td');
+            rows[0].querySelector('code').textContent = rsrc.type;
+            rows[1].textContent = rsrc.id;
+            rows[2].textContent = rsrc.data.length;
+            rows[3].textContent = rsrc.name || "";
+            const a = rows[4].querySelector('a');
+            a.href = URL.createObjectURL(new Blob(
+                [ rsrc.data ],
+                { type: 'application/octet-stream' }
+            ));
+            a.download = `${res.name}.${rsrc.type}`;
+            tbody.append(tr);
+        }
+
+        details.classList.remove('hidden');
+    }
+    catch(e) {
+        error.classList.remove('hidden');
+        if (typeof e === "string") {
+            error.querySelector('p').textContent = e;
+        }
+        else {
+            error.querySelector('p').textContent = e.message;
+        }
+    }
+}
+
+function setField(name, value) {
+    document.getElementById(name).textContent = value;
+}
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const details = document.getElementById('details');
+    const error = document.getElementById('error');
+
+    document.forms[0].addEventListener('submit', async (e) => readMacbinary(e, details, error));
+
+    run()
+        .then(() => document.forms[0]['read'].disabled = false)
+        .catch(() => {
+            error.classList.remove('hidden');
+            error.querySelector('p').textContent = "Unable to initialise WASM module.";
+        });
+});
